f95b75dcf5fb0e450a3a78a2c1e2c5032a76ff54
Erez Heilweil <erezhe@gmail.com>
2023-01-24 13:18:55 +0000

    Added s3-3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = "my-s3-bucket"
   acl    = "private"
-
+  version = "2.8.0"
   versioning = {
     enabled = true
   }
